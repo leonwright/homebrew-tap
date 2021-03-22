dd88b42e08a201d4946680833a9716969d21121f
goreleaserbot <goreleaser@carlosbecker.com>
2021-03-22 04:58:09 -0500

    Brew formula update for reactoragent version v0.1.0-alpha7

--- Formula/reactoragent.rb
+++ Formula/reactoragent.rb
@@ -5,24 +5,24 @@
 class Reactoragent < Formula
   desc "Supercharge your development server."
   homepage "https://nerderbur.tech"
-  version "0.1.0-alpha6"
+  version "0.1.0-alpha7"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/leonwright/reactor-agent/releases/download/v0.1.0-alpha6/reactoragent_0.1.0-alpha6_Darwin_x86_64.tar.gz"
-    sha256 "30ab24a644e3226d9bac1b6e9e41a25a09aed3b74a3f379e8622deb55dc64dd7"
+    url "https://github.com/leonwright/reactor-agent/releases/download/v0.1.0-alpha7/reactoragent_0.1.0-alpha7_Darwin_x86_64.tar.gz"
+    sha256 "d63f2e3ec987f892cfd75a213148dc542350671342beb8c56436bd06d39b8e8f"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/leonwright/reactor-agent/releases/download/v0.1.0-alpha6/reactoragent_0.1.0-alpha6_Darwin_arm64.tar.gz"
-    sha256 "56c0044879a8c630e0bfad5e6dec982132dd5b7bbdde4bba351afd9fa5bb976c"
+    url "https://github.com/leonwright/reactor-agent/releases/download/v0.1.0-alpha7/reactoragent_0.1.0-alpha7_Darwin_arm64.tar.gz"
+    sha256 "e7907f695b800c01cc6d49dc42e385d879d98d974371dd92ae95da8ce55a99dd"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/leonwright/reactor-agent/releases/download/v0.1.0-alpha6/reactoragent_0.1.0-alpha6_Linux_x86_64.tar.gz"
-    sha256 "1742f2856bb9982be3e5294af94cdd0b7ba3925a9796f93f6e631c3a233a66b2"
+    url "https://github.com/leonwright/reactor-agent/releases/download/v0.1.0-alpha7/reactoragent_0.1.0-alpha7_Linux_x86_64.tar.gz"
+    sha256 "80815ff22d3714a17c8b9037ffbce2dcc0bfda378accd68d32edce3866a0e92d"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/leonwright/reactor-agent/releases/download/v0.1.0-alpha6/reactoragent_0.1.0-alpha6_Linux_arm64.tar.gz"
-    sha256 "f97633851e8844c69f7541d342546eed2781a8ca5a1aec00f1301e91d650eeb5"
+    url "https://github.com/leonwright/reactor-agent/releases/download/v0.1.0-alpha7/reactoragent_0.1.0-alpha7_Linux_arm64.tar.gz"
+    sha256 "e2ed9378a60850a95b8db06699b3ad2047f77488ddd6c030aed6b535fe42e297"
   end
 
   def install
